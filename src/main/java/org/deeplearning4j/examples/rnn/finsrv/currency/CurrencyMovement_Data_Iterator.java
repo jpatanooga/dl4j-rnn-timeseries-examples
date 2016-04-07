e64f35489bb86bd3627acec2c3a9119f6df6380b
Josh Patterson <jpatterson@floe.tv>
2016-04-07 16:16:54 -0400

    some quick cleanup

--- src/main/java/org/deeplearning4j/examples/rnn/finsrv/currency/CurrencyMovement_Data_Iterator.java
+++
@@ -1,5 +0,0 @@
-package org.deeplearning4j.examples.rnn.finsrv.currency;
-
-public class CurrencyMovement_Data_Iterator {
-
-}
--- src/main/java/org/deeplearning4j/examples/rnn/finsrv/currency/CurrencyMovement_LSTM_Model.java
+++
@@ -1,5 +0,0 @@
-package org.deeplearning4j.examples.rnn.finsrv.currency;
-
-public class CurrencyMovement_LSTM_Model {
-
-}
